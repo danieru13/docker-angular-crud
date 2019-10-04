b22fa0aff37502a98280bdb9d6f68705dc547aa6
Daniel Vanegas <danieru1313@gmail.com>
2019-10-03 22:24:22 -0500

    Finish roman-numerals service and add testing cases

--- src/app/roman-numerals.service.spec.ts
+++ src/app/roman-numerals.service.spec.ts
@@ -34,4 +34,69 @@ describe('RomanNumeralsService', () => {
     const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
     expect(service.arabicToRoman(5)).toEqual('V');
   });
+
+  it(`Seis 6 debe ser igual a 'VI'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(6)).toEqual('VI');
+  });
+
+  it(`Nueve 9 debe ser igual a 'IX'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(9)).toEqual('IX');
+  });
+
+  it(`Diez 10 debe ser igual a 'X'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(10)).toEqual('X');
+  });
+
+  it(`Catorce 14 debe ser igual a 'XIV'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(14)).toEqual('XIV');
+  });
+
+  it(`Veinte 20 debe ser igual a 'XX'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(20)).toEqual('XX');
+  });
+
+  it(`Cuarenta 40 debe ser igual a 'XL'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(40)).toEqual('XL');
+  });
+
+  it(`Cincuenta 50 debe ser igual a 'L'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(50)).toEqual('L');
+  });
+
+  it(`Noventa 90 debe ser igual a 'XC'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(90)).toEqual('XC');
+  });
+
+  it(`Cien 100 debe ser igual a 'C'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(100)).toEqual('C');
+  });
+
+  it(`Cuatrocientos 400 debe ser igual a 'CD'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(400)).toEqual('CD');
+  });
+
+  it(`Quinientos 500 debe ser igual a 'D'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(500)).toEqual('D');
+  });
+
+  it(`Novecientos 900 debe ser igual a 'CM'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(900)).toEqual('CM');
+  });
+
+  it(`Mil 1000 debe ser igual a 'M'`, () => {
+    const service: RomanNumeralsService = TestBed.get(RomanNumeralsService);
+    expect(service.arabicToRoman(1000)).toEqual('M');
+  });
 });
--- src/app/roman-numerals.service.ts
+++ src/app/roman-numerals.service.ts
@@ -8,13 +8,13 @@ export class RomanNumeralsService {
   constructor() { }
 
   arabicToRoman(arabic: number): String {
-    const romanNumbers = ["V", "IV", "I"];
-    const arabicNumbers = [5, 4, 1];
+    const romanNumbers = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    const arabicNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
     let roman = ""
 
-    for(let i = 0; i < arabicNumbers.length; i++){
-      while(arabicNumbers[i] <= arabic) {
+    for (let i = 0; i < arabicNumbers.length; i++) {
+      while (arabicNumbers[i] <= arabic) {
         roman += romanNumbers[i];
         arabic -= arabicNumbers[i];
       }
